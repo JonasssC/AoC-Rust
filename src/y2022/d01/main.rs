e9327a93f56f3bbbcd1e73b8218771d9ad80c70d
JonasssC <jonas.couwberghs@student.kuleuven.be>
2022-12-01 10:29:30 +0100

    2022 Day 1

--- src/y2022/d01/main.rs
+++ src/y2022/d01/main.rs
@@ -1,15 +1,15 @@
-use aoc_rust::{ read_split_on_empty_line };
+use aoc_rust::{ read_lines_split_on_empty_line_as_i32 };
 
 fn main() {
-    let elfs = read_split_on_empty_line(2022, 1);
+    let elfs = read_lines_split_on_empty_line_as_i32(2022, 1);
     println!("Part 1: {}", part1(elfs.clone()));
     println!("Part 2: {}", part2(elfs.clone()));
 }
 
-fn part1(elfs: Vec<String>) -> i32 {
+fn part1(elfs: Vec<Vec<i32>>) -> i32 {
     let mut most = 0;
     for elf in elfs.iter() {
-        let n = count_calories(elf);
+        let n = elf.iter().sum();
         if n > most {
             most = n;
         }
@@ -17,21 +17,15 @@ fn part1(elfs: Vec<String>) -> i32 {
     return most;
 }
 
-fn part2(elfs: Vec<String>) -> i32 {
+fn part2(elfs: Vec<Vec<i32>>) -> i32 {
     let mut weights: Vec<i32> = Vec::new();
 
     for elf in elfs.iter() {
-        weights.push(count_calories(elf));
+        weights.push(elf.iter().sum());
     }
 
     weights.sort();
     weights.reverse();
 
     return weights[0] + weights[1] + weights[2];
-}
-
-fn count_calories(elf: &String) -> i32 {
-    elf.split("\n")
-        .map(| s | s.parse::<i32>().unwrap())
-        .sum()
 }
