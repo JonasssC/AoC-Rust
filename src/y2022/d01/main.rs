ee244edf2ab225945d9d00c58c2361b465b25640
JonasssC <jonas.couwberghs@student.kuleuven.be>
2022-12-01 06:14:44 +0100

    2022 Day 1

---
+++ src/y2022/d01/main.rs
@@ -0,0 +1,39 @@
+use aoc_rust::{ read_split_on_empty_line };
+
+fn main() {
+    let elfs = read_split_on_empty_line(2022, 1);
+    println!("{:?}", elfs);
+
+    println!("Part 1: {}", part1(elfs.clone()));
+    println!("Part 2: {}", part2(elfs.clone()));
+}
+
+fn part1(elfs: Vec<String>) -> i32 {
+    let mut most = 0;
+    for elf in elfs.iter() {
+        let n = count_calories(elf);
+        if n > most {
+            most = n;
+        }
+    }
+    return most;
+}
+
+fn part2(elfs: Vec<String>) -> i32 {
+    let mut weights: Vec<i32> = Vec::new();
+
+    for elf in elfs.iter() {
+        weights.push(count_calories(elf));
+    }
+
+    weights.sort();
+    weights.reverse();
+
+    return weights[0] + weights[1] + weights[2];
+}
+
+fn count_calories(elf: &String) -> i32 {
+    elf.split("\n")
+        .map(| s | s.parse::<i32>().unwrap())
+        .sum()
+}
