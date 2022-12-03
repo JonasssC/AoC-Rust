955592aac4899acf28a4cf6cfc29ba1243215942
JonasssC <jonas.couwberghs@student.kuleuven.be>
2022-12-03 21:06:54 +0100

    2022 Day 3

--- src/util/string.rs
+++ src/util/string.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! common_chars {
-    ($s1:expr, $($args:expr), *) => {{
+    ($s1:expr, $($args:expr), +) => {{
         let mut res: Vec<char> = $s1.chars().collect();
         $(
             let mut common: Vec<char> = Vec::new();
