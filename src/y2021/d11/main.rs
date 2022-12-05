0eae0ee71c6a2584402cbb258e68a4a52d628a89
JonasssC <jonas.couwberghs@student.kuleuven.be>
2022-12-05 01:23:59 +0100

    2021 Day 11

---
+++ src/y2021/d11/main.rs
@@ -0,0 +1,89 @@
+use aoc_rust::util::input::read_lines;
+
+fn main() {
+    let octos: Vec<Vec<i32>> = read_lines(2021, 11).iter()
+        .map(| l | l.chars()
+            .map(| c | c as i32 - '0' as i32)
+            .collect()
+        )
+        .collect();
+    println!("Part 1: {}", part1(octos.clone()));
+    println!("Part 2: {}", part2(octos.clone()));
+}
+
+fn get_coords_vec(octos: Vec<Vec<i32>>) -> Vec<(usize, usize)> {
+    let mut coords: Vec<(usize, usize)> = Vec::new();
+
+    for i in 0..octos.len() {
+        for j in 0..octos[i].len() {
+            coords.push((i, j));
+        }
+    }
+
+    return coords;
+}
+
+fn is_in_field(octos: &mut Vec<Vec<i32>>, row: i32, col: i32) -> bool {
+    row >= 0 && row < octos.len() as i32
+        && col >= 0 && col < octos[0].len() as i32
+}
+
+fn flash_octo(octos: &mut Vec<Vec<i32>>, row: usize, col: usize, flash_count: &mut i32) {
+    if octos[row][col] > 9 {
+        octos[row][col] = -100;
+        *flash_count += 1;
+
+        for i in 0..3 {
+            for j in 0..3 {
+                if !(i == 1 && j == 1) && is_in_field(octos, row as i32 - 1 + i as i32, col as i32 - 1 + j as i32) {
+                    octos[row + i - 1][col + j - 1] += 1;
+                    flash_octo(octos, row + i - 1, col + j - 1, flash_count);
+                }
+            }
+        }
+    }
+}
+
+fn part1(mut octos: Vec<Vec<i32>>) -> i32 {
+    let mut flash_count = 0;
+    let coords = get_coords_vec(octos.clone());
+
+    for _ in 0..100 {
+        for coord in coords.iter() {
+            octos[coord.0][coord.1] += 1;
+        }
+        for coord in coords.iter() {
+            flash_octo(&mut octos, coord.0, coord.1, &mut flash_count);
+        }
+        for coord in coords.iter() {
+            if octos[coord.0][coord.1] < 0 {
+                octos[coord.0][coord.1] = 0;
+            }
+        }
+    }
+
+    return flash_count;
+}
+
+fn part2(mut octos: Vec<Vec<i32>>) -> i32 {
+    let mut i = 0;
+    let coords = get_coords_vec(octos.clone());
+
+    loop {
+        i += 1;
+        for coord in coords.iter() {
+            octos[coord.0][coord.1] += 1;
+        }
+        for coord in coords.iter() {
+            flash_octo(&mut octos, coord.0, coord.1, &mut 0);
+        }
+        for coord in coords.iter() {
+            if octos[coord.0][coord.1] < 0 {
+                octos[coord.0][coord.1] = 0;
+            }
+        }
+        if coords.iter().map(| c | octos[c.0][c.1]).max().unwrap() == 0 {
+            return i;
+        }
+    }
+}
