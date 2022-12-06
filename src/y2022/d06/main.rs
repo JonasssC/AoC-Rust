5c17f4285ed8e35eca287de98c8b2a394aee64ae
JonasssC <jonas.couwberghs@student.kuleuven.be>
2022-12-06 06:26:04 +0100

    2022 Day 6

---
+++ src/y2022/d06/main.rs
@@ -0,0 +1,31 @@
+use aoc_rust::util::input::read_string;
+
+fn main() {
+    let code = read_string(2022, 6);
+    println!("Part 1: {}", part1(code.clone()));
+    println!("Part 2: {}", part2(code.clone()));
+}
+
+fn part1(code: String) -> usize {
+    solve(code, 4)
+}
+
+fn part2(code: String) -> usize {
+    solve(code, 14)
+}
+
+fn solve(code: String, size: usize) -> usize {
+    'outerloop: for i in 0..code.len()-size {
+        let sub = code.chars().collect::<Vec<char>>()[i..i+size].to_vec();
+
+        for j in 0..size {
+            if sub.iter().filter(| &&c | sub[j] == c).count() != 1 {
+                continue 'outerloop;
+            }
+        }
+
+        return i + size;
+    }
+
+    0
+}
