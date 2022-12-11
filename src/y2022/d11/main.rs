75e20c461c2ef6e06843564ee976992532870b39
JonasssC <jonas.couwberghs@student.kuleuven.be>
2022-12-11 14:54:11 +0100

    2022 Day 11

--- src/y2022/d11/main.rs
+++ src/y2022/d11/main.rs
@@ -5,7 +5,7 @@ use aoc_rust::util::string::regex_parse;
 #[derive(Debug, Clone)]
 struct Monkey {
     items: Vec<i64>,
-    operation: String,
+    operation: Vec<String>,
     test_divisible_by: i64,
     test_true_to: usize,
     test_false_to: usize
@@ -23,7 +23,7 @@ impl Monkey {
                         .split(", ")
                         .map(| n | n.parse::<i64>().unwrap())
                         .collect(),
-                    operation: lines[2].clone(),
+                    operation: regex_parse::<String>(&lines[2], r"  Operation: new = (.+) (\+|\*) (.+)"),
                     test_divisible_by: regex_parse::<i64>(&lines[3], r"Test: divisible by (\d+)")[0].clone(),
                     test_true_to: regex_parse::<usize>(&lines[4], r"If true: throw to monkey (\d+)")[0],
                     test_false_to: regex_parse::<usize>(&lines[5], r"If false: throw to monkey (\d+)")[0],
@@ -34,11 +34,10 @@ impl Monkey {
     }
 }
 
-fn perform_operation(operation: String, old: i64) -> i64 {
-    let groups = regex_parse::<String>(&operation, r"  Operation: new = (.+) (\+|\*) (.+)");
-    let n1 = if &groups[0] == "old" { old } else { groups[0].parse::<i64>().unwrap() };
-    let n2 = if &groups[2] == "old" { old } else { groups[2].parse::<i64>().unwrap() };
-    if &groups[1] == "+" {
+fn perform_operation(operation: Vec<String>, old: i64) -> i64 {
+    let n1 = if &operation[0] == "old" { old } else { operation[0].parse::<i64>().unwrap() };
+    let n2 = if &operation[2] == "old" { old } else { operation[2].parse::<i64>().unwrap() };
+    if &operation[1] == "+" {
         n1 + n2
     } else {
         n1 * n2
@@ -131,6 +130,6 @@ fn part2(mut monkeys: Vec<Monkey>) -> usize {
         }
     }
 
-    let max_2 = max_n(inspect_count, 2);
-    max_2[0] * max_2[1]
+    let max = max_n(inspect_count, 2);
+    max[0] * max[1]
 }
